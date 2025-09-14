fb265179a362ca403830f9351456669248d7ad74
idriss <inscrag@gmail.com>
2025-09-14 03:01:04 +0200

    update domaine for images

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       "localhost", // for local Strapi
       "127.0.0.1",
       "images.unsplash.com",
-      "timely-wealth-923d9aeb3d.strapiapp.com", // replace with your production domain
+      "timely-wealth-923d9aeb3d.media.strapiapp.com", // replace with your production domain
     ],
   },
 };

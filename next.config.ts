6c1d66aded5295a66679991e437300707361a11e
idriss <inscrag@gmail.com>
2025-09-14 02:55:47 +0200

    add domaine for images

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       "localhost", // for local Strapi
       "127.0.0.1",
       "images.unsplash.com",
-      "your-production-domain.com", // replace with your production domain
+      "timely-wealth-923d9aeb3d.strapiapp.com", // replace with your production domain
     ],
   },
 };

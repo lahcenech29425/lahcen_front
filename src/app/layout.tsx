fed1acf5edbd94277401a28ef8144d1413f44930
idriss <inscrag@gmail.com>
2025-10-16 20:29:35 +0200

    delete + sign in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,28 +94,26 @@ export default async function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <head>
-        + <link rel="icon" href="/favicon.ico" />+{" "}
+        <link rel="icon" href="/favicon.ico" />{" "}
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="/favicon-32x32.png"
-        />
-        +{" "}
+        />{" "}
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
-        />
-        +{" "}
+        />{" "}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        + <link rel="manifest" href="/site.webmanifest" />
-        + <meta name="theme-color" content="#0f172a" />+{" "}
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#0f172a" />{" "}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
